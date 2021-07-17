d7e0d04105e905d0b1c6a7b512e6c61ef4cd6ee6
Akhil Joy <b2akhilmj@gmail.com>
2021-07-17 16:33:41 +0530

    Fav list

--- src/Components/Pages/LeadListing/LeadDetails/Tabs/SingleActivity.js
+++ src/Components/Pages/LeadListing/LeadDetails/Tabs/SingleActivity.js
@@ -1,31 +1,18 @@
 import moment from "moment"
+import "./SingleActivity.css"
 
 const SingleActivity = (props) => {
     let date = props.item.log_date;
     date = moment(date).fromNow();
     return <div className="row">
         <div className="col-md-2">
-            <div className="message-day backday"> {date} </div>
+            <div className="message-day backday">
+            <p className="left-time">{date} <br/>{props.item.user.name} <br/> <span className="badge badge-primary">{props.item.log_type}</span></p>
+            </div>
         </div>
         <div className="col-md-10">
             <div className="message-item" id="m16">
                 <div className="message-inner">
-                    <div className="message-head clearfix">
-                        <div className="user-detail">
-                            <h5 className="handle">{props.item.user.name}</h5>
-                            <div className="post-meta">
-                                <div className="asker-meta">
-                                    <span className="qa-message-what"></span>
-                                    <span className="qa-message-when">
-
-                              </span>
-                                    <span className="qa-message-who">
-                              <span className="qa-message-who-data">{props.item.log_type}</span>
-                              </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div className="qa-message-content">
                         {props.item.description}
                     </div>
